11f8234e40450ac665ca0c17629fe3982e2c78f0
Rajnish Singh Thakur <74904062+RajnishProgrammer@users.noreply.github.com>
2023-03-26 20:44:39 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from myapp import create_app
 
 app = create_app()
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=os.getenv("PORT", default=5000))
