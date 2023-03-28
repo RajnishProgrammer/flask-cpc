4581b4be3bd10a822ef122d970e6227e8f71c632
Rajnish Singh Thakur <74904062+RajnishProgrammer@users.noreply.github.com>
2023-03-29 01:56:39 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard-to-guess-string'
     UPLOAD_FOLDER = os.path.join('myapp', 'static', 'uploads')
     print(UPLOAD_FOLDER)
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB
+    MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 100MB
